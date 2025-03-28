f16a4bb73b0edb8e362c4f4c3a82b4c74408381a
harryof <159545579+harryof@users.noreply.github.com>
2025-03-28 21:42:50 +0300

    Add files via upload

---
+++ task-component.js
@@ -0,0 +1,26 @@
+import {createElement} from '../framework/render.js';
+
+function createTaskTemplate(task) {
+  return `<li class="task">${task}</li>`;
+}
+
+export default class TaskComponent {
+  constructor(task) {
+    this.task = task;
+  }
+
+  getTemplate() {
+    return createTaskTemplate(this.task);
+  }
+
+  getElement() {
+    if (!this.element) {
+      this.element = createElement(this.getTemplate());
+    }
+    return this.element;
+  }
+
+  removeElement() {
+    this.element = null;
+  }
+}
